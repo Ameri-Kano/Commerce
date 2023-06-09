e522e95b28ee66efa310026873f39730e92d5e07
Ameri-Kano <yeokkim13@naver.com>
2023-06-09 19:17:31 +0900

    feat: [EMAIL-VERIFICATION-002] SignUpCustomerService 구현 완료 및 인증 처리 SignUpCustomerService.verifyEmail) 완료

--- user-api/src/main/java/com/amerikano/cms/user/application/SignUpApplication.java
+++ user-api/src/main/java/com/amerikano/cms/user/application/SignUpApplication.java
@@ -1,27 +1,58 @@
 package com.amerikano.cms.user.application;
 
 import com.amerikano.cms.user.client.MailgunClient;
+import com.amerikano.cms.user.client.mailgun.SendMailForm;
 import com.amerikano.cms.user.domain.SignUpForm;
 import com.amerikano.cms.user.domain.model.Customer;
 import com.amerikano.cms.user.exception.CustomException;
 import com.amerikano.cms.user.exception.ErrorCode;
 import com.amerikano.cms.user.service.SignUpCustomerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class SignUpApplication {
     private final MailgunClient mailgunClient;
     private final SignUpCustomerService signUpCustomerService;
 
-    public void customerSignUp(SignUpForm form) {
+    public void customerVerify(String email, String code) {
+        signUpCustomerService.verifyEmail(email, code);
+    }
+
+    public String customerSignUp(SignUpForm form) {
         if(signUpCustomerService.isEmailExist(form.getEmail())) {
             throw new CustomException(ErrorCode.ALREADY_REGISTERED_USER);
         } else {
             Customer c = signUpCustomerService.signUp(form);
+
+            String code = getRandomCode();
+            SendMailForm sendMailForm = SendMailForm.builder()
+                    .from("test@gmail.com")
+                    .to(form.getEmail())
+                    .subject("Verification Email!")
+                    .text(getVerificationEmailBody(form.getEmail(), form.getName(), getRandomCode()))
+                    .build();
+            log.info(mailgunClient.sendEmail(sendMailForm).getBody());
+
+            signUpCustomerService.changeCustomerValidateEmail(c.getId(), code);
+            return "회원 가입에 성공하였습니다.";
         }
     }
 
+    private String getRandomCode() { return RandomStringUtils.random(10, true, true); }
 
+    private String getVerificationEmailBody(String email, String name, String code) {
+        StringBuilder builder = new StringBuilder();
+        return builder.append("Hello ").append(name).append("! Please Click Link for verification.\n\n")
+                .append("http://localhost:8081/signup/verify/customer?email=")
+                .append(email)
+                .append("&code=")
+                .append(code).toString();
+    }
 }
--- user-api/src/main/java/com/amerikano/cms/user/service/SignUpCustomerService.java
+++ user-api/src/main/java/com/amerikano/cms/user/service/SignUpCustomerService.java
@@ -3,16 +3,22 @@ package com.amerikano.cms.user.service;
 import com.amerikano.cms.user.domain.CustomerRepository;
 import com.amerikano.cms.user.domain.SignUpForm;
 import com.amerikano.cms.user.domain.model.Customer;
+import com.amerikano.cms.user.exception.CustomException;
+import com.amerikano.cms.user.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Locale;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SignUpCustomerService {
 
     private final CustomerRepository customerRepository;
+
     public Customer signUp(SignUpForm form) {
         return customerRepository.save(Customer.from(form));
     }
@@ -20,5 +26,36 @@ public class SignUpCustomerService {
     public boolean isEmailExist(String email) {
         return customerRepository.findByEmail(email.toLowerCase(Locale.ROOT))
                 .isPresent();
+    }
+
+    @Transactional
+    public void verifyEmail(String email, String code) {
+        Customer customer = customerRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+
+        if (customer.isVerify()) {
+            throw new CustomException(ErrorCode.ALREADY_VERIFIED);
+        }
+        if (!customer.getVerificationCode().equals(code)) {
+            throw new CustomException(ErrorCode.WRONG_VERIFICATION);
+        }
+        if (customer.getVerifyExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new CustomException(ErrorCode.CODE_EXPIRED);
+        }
+        customer.setVerify(true);
+    }
+
+    @Transactional
+    public LocalDateTime changeCustomerValidateEmail(Long customerId, String verificationCode) {
+        Optional<Customer> customerOptional = customerRepository.findById(customerId);
+
+        if (customerOptional.isPresent()) {
+            Customer customer = customerOptional.get();
+            customer.setVerificationCode(verificationCode);
+            customer.setVerifyExpiredAt(LocalDateTime.now().plusDays(1));
+            return customer.getVerifyExpiredAt();
+        }
+
+        throw new CustomException(ErrorCode.NOT_FOUND_USER);
     }
 }
